ca9dd1f8c03f1fa670583516080f6865f8b399d0
Tofan_Timotei <tofan.simon21@gmail.com>
2025-05-16 17:27:10 +0200

    nothing special to commit

--- 05-travel-list/far-away/src/App.jsx
+++ 05-travel-list/far-away/src/App.jsx
@@ -1,10 +1,7 @@
 import { useState } from "react";
 
 function App() {
-  const [initialItems, setInitialItems] = useState([
-    { id: 1, description: "Passports", quantity: 2, packed: false },
-    { id: 2, description: "Socks", quantity: 12, packed: false },
-  ]);
+  const [initialItems, setInitialItems] = useState([]);
 
   return (
     <div className="app">
