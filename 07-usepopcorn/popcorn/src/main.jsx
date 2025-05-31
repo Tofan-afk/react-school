ea20d600b3d00a00bad22055490ef9b5708e8c58
Tofan_Timotei <tofan.simon21@gmail.com>
2025-05-31 13:14:39 +0200

    done with usePopcorn

---
+++ 07-usepopcorn/popcorn/src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+//import App from "./App.jsx";
+//import "./index.css";
+import StarRating from "./StarRating";
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <StarRating maxRating={5} />
+  </StrictMode>
+);
